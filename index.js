2b8023ebbea2cca156f4f1363edff893d4e2bc34
Kirill <kirillsavorin@gmail.com>
2025-01-09 22:17:14 +0100

    express advanced

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-import http from 'http';
-import express from 'express';
+import express from "express";
+import fs from "fs/promises";
 
 const app = express();
 
-app.get('/hello', (request, response) => {
-    const name = request.query.firstName || 'world';
-    response.send(`Hello, world, ${name}`);
+app.get('/', async (request, response) => {
+    const buf = await fs.readFile('./static/index.html');
+    const html = buf.toString();
+    response.send(html);
 });
 
-app.get('/goodbye', (request, response) => {
-    response.send('Goodbye, world');
+app.use('/static', express.static('./static'));
+
+app.get('/:name', async (request, response) => {
+    const buf = await fs.readFile('./static/index.html');
+    const html = buf.toString();
+    const name = request.params.name;
+
+    const changedHtml = html.replace('world', name);
+
+    response.send(changedHtml);
 });
 
-app.listen(3080)
+app.listen(3080);
+// app.get('/*', async (request, response) => {
+//   try {
+//     const path = request.path;
+//     const fileName = `./static${path}`;
+//     const buf = await fs.readFile(fileName);
+//     const text = buf.toString();
+
+//     const suffix = path.split('.')[1];
+//     response.setHeader('Content-Type', 'text/' + suffix);
+//     response.send(text);
+//   } catch (err) {
+//     response.status(404);
+//     // responce.end();
+//   }
+// });
+
+// app.get('/index.html', async (request, response) => {
+//     const buf = await fs.readFile('./static/index.html');
+//     const html = buf.toString();
+
+//     response.send(html);
+// });
+
+// app.get('/main.css', async (request, response) => {
+//     const buf = await fs.readFile('./static/main.css');
+//     const css = buf.toString();
+
+//     response.setHeader('Content-Type', 'text/css')
+//     response.send(css);
+// });
